8af5c14d39a4be1b768f153394ee6e9d838c64a5
Rajwinder-Kaur23 <gillrajni2306@gmail.com>
2025-06-11 20:01:46 -0700

    Update Login.js

--- pages/Login.js
+++ pages/Login.js
@@ -1,11 +1,1 @@
-import React, { useState } from 'react';
 
-function Login() {
-  const [email, setEmail] = useState('');
-  const [password, setPassword] = useState('');
-
-  const handleLogin = (e) => {
-    e.preventDefault();
-    console.log('Login with', email, password);
-    // Later: send request to backend
-  };
