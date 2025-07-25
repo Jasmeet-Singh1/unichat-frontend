2d34eb22109db03d78d99715a6457e9b049e5ca2
Sudeepta Bosu Sukhi <sudeeptabosu@Sudeeptas-MacBook-Pro.local>
2025-07-25 13:10:19 -0700

    Amin Commit

--- src/App.js
+++ src/App.js
@@ -1,8 +1,29 @@
-import React from "react";
+//import React from "react";
 import Login from "./pages/Login.js";
+import ChatContainer from "./components/chat/ChatContainer.jsx";
+import ChatWindow from "./components/chat/ChatWindow.jsx";
+import Message from "./components/chat/Message.jsx";
+import MessageInput from "./components/chat/MessageInput.jsx";
+import MessageList from "./components/chat/MessageList.jsx";
+import TypingIndicator from "./components/chat/TypingIndicator.jsx";
 
+      setTypingUsers(prev => {
+        const updatedTypingUsers = new Set(prev);
+        updatedTypingUsers.add(userId);
+        return updatedTypingUsers;
+      });
 function App() {
-  return <Login />;
+  return (
+    <>
+      <Login />
+      <ChatContainer />
+      <ChatWindow />
+      <Message />
+      <MessageInput />
+      <MessageList />
+      <TypingIndicator />
+    </>
+  );
 }
 
 export default App;
