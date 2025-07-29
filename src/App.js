a66cdf65ac3cb1bbc96dbaa3a9df0c7544981d54
Rajwinder-Kaur23 <gillrajni2306@gmail.com>
2025-07-28 19:03:33 -0700

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,20 +1,14 @@
-import React from 'react';
+import { Routes, Route, Navigate } from "react-router-dom";
 import Login from './pages/Login';
 import Signup from './pages/Signup';
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
-import Dashboard from './pages/Dashboard';
+
 function App() {
   return (
-    <BrowserRouter>
-      <div>
-        <Routes>
-          <Route exact path='/' element={<Login />} />
-          <Route exact path='/login' element={<Login />} />
-          <Route exact path='/signup' element={<Signup />} />
-          <Route path='/dashboard' element={<Dashboard />} />
-        </Routes>
-      </div>
-    </BrowserRouter>
+    <Routes>
+      <Route path="/" element={<Navigate to="/login" />} />
+      <Route path="/login" element={<Login />} />
+      <Route path="/signup" element={<Signup />} />
+    </Routes>
   );
 }
 
